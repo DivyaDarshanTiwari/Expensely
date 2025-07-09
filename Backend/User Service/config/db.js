a6dfb28c296a217f842586dde2c042eeb930ab2f
Vilsium <vilsium28@gmail.com>
2025-07-09 16:24:17 +0530

    Changes regarding hosted database

--- Backend/User Service/config/db.js
+++ Backend/User Service/config/db.js
@@ -3,11 +3,11 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: 5432,
+  port: process.env.DB_PORT,
 });
 
 module.exports = { pool };
