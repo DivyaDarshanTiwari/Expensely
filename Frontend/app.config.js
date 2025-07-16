1cf7ee1e8d7d93521e89a48e2424d9e37099c3f3
Vilsium <vilsium28@gmail.com>
2025-07-16 14:21:26 +0530

    package installation, changes for apk building

--- Frontend/app.config.js
+++ Frontend/app.config.js
@@ -4,16 +4,17 @@ export default ({ config }) => {
   return {
     ...config,
     expo: {
-      "owner": "vilsium",
+      owner: "vilsium",
       extra: {
-        "eas" : {
-          "projectId": "6d9d48a7-9f2b-4ec8-a289-0cf50de66997"
+        eas: {
+          projectId: "6d9d48a7-9f2b-4ec8-a289-0cf50de66997",
         },
         Basic_URL: process.env.Basic_URL,
         Group_URL: process.env.Group_URL,
         User_URL: process.env.User_URL,
         OCR_URL: process.env.OCR_URL,
       },
+      plugins: ["expo-secure-store"],
     },
   };
 };
