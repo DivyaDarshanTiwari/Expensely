3b50d43d3c285a9370935b59b8180d1dc6ce5b5b
DivyaDarshanTiwari <davidrocco400@outlook.com>
2025-08-10 20:12:54 +0530

    added cluster and made the policy round robin

--- Backend/User Service/server.js
+++ Backend/User Service/server.js
@@ -1,9 +1,25 @@
 require("dotenv").config();
 
+const cluster = require("cluster");
+const os = require("os");
 const { app } = require("./app");
 
-const port = process.env.SERVER_PORT || 5000;
+const cpu = os.cpus().length;
 
-app.listen(port, () => {
-  console.log("Server is running on port" + port);
-});
+cluster.schedulingPolicy = cluster.SCHED_RR;
+
+if (cluster.isPrimary) {
+  for (let i = 0; i < cpu; i++) {
+    cluster.fork();
+  }
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+    cluster.fork();
+  });
+} else {
+  const port = process.env.SERVER_PORT || 5000;
+
+  app.listen(port, () => {
+    console.log("Server is running on port" + port + "on pid" + process.pid);
+  });
+}
