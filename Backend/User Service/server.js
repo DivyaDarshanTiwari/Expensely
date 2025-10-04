1d65c397e47144d57c88d283e370496a41871da4
DivyaDarshanTiwari <davidrocco400@outlook.com>
2025-10-05 03:49:28 +0530

    made minor changes in package..json dockerignore and server.js

--- Backend/User Service/server.js
+++ Backend/User Service/server.js
@@ -1,25 +1,9 @@
-require("dotenv").config();
+require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
 
-const cluster = require("cluster");
-const os = require("os");
 const { app } = require("./app");
 
-const cpu = os.cpus().length;
+const port = process.env.SERVER_PORT || 5000;
 
-cluster.schedulingPolicy = cluster.SCHED_RR;
-
-if (cluster.isPrimary) {
-  for (let i = 0; i < cpu; i++) {
-    cluster.fork();
-  }
-  cluster.on("exit", (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`);
-    cluster.fork();
-  });
-} else {
-  const port = process.env.SERVER_PORT || 5000;
-
-  app.listen(port, () => {
-    console.log("Server is running on port" + port + "on pid" + process.pid);
-  });
-}
+app.listen(port, () => {
+  console.log("Server is running on port" + port + "on pid" + process.pid);
+});
